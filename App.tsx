7b3311fc0499e263872acf2073b43132e5603a70
Indra Lukmana <indralukmana@live.com>
2019-10-24 20:58:23 +0700

    ✨new: start game screen component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Header from './components/Header'
+import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
   return (
     <View style={styles.screen}>
+      <StartGameScreen />
       <Header title='Number Guess' />
     </View>
   )
